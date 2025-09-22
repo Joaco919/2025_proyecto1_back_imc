69a92198401e149cfb766c2fb2ba04b07e62f79d
lucasleone03 <lucasagustinleone2003@gmail.com>
2025-09-21 21:47:47 -0300

    Actualizar configuración de CORS para usar la URL del frontend en producción

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Habilitar CORS para el frontend local o usando la variable FRONTEND_URL
-  const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:5173';
+  // const frontendUrl = 'http://localhost:5173';
+  const frontendUrl = 'https://2025-proyecto1-front-imc-sepia.vercel.app';
   app.enableCors({
     origin: frontendUrl,
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
